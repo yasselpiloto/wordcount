46aeae4e48a0fa9f2f6e2e1dd26091063cdbe025
pilo <yasselpiloto@gmail.com>
2022-07-02 10:13:20 -0400

    implement wordcount

--- axios_client.py
+++ axios_client.py
@@ -3,22 +3,33 @@ import requests
 
 class AxiosClient:
 
-    def __init__(self, story_stream_url):
-        self.story_stream_url = story_stream_url
+    def __init__(self, news_api_url):
+        self.news_api_url = news_api_url
 
-    # gets the Ids for the first n articles from the story stream
-    def get_articles(self, count):
-        next_page = self.story_stream_url
+    # gets the Ids for the first n pages from the story stream
+    def get_pages(self, pages = 1):
         results = []
+        next_page = f'{self.news_api_url}/stream/content/'
 
-        # iterates pages of the story stream until enough article Ids are fetched
-        while len(results) < count and next_page is not None:
+        # iterates the first N pages of the story stream
+        while len(results) < pages * 10 and next_page is not None:
             response = requests.get(next_page).json()
-            results += response['results'][0:10]
+            results += response['results']
             next_page = response['next']
-
-        # removes article Ids that are in excess
-        results = results[:-(len(results) - count)]
 
         return results
 
+    # returns a dictionary with the wordcount and reading time for each story
+    def get_summary(self, story_ids):
+        result = {}
+
+        for story_id in story_ids:
+            response = requests.get(f'{self.news_api_url}/content/{story_id}/').json()
+            result[story_id] = {'wordcount': response['wordcount']}
+
+        return result
+
+
+# parsed = requests.get('https://api.axios.com/api/render/content/aa030df2-0a82-4066-bfba-7aa2ef316b75/').json()
+# print(print(json.dumps(parsed, indent=4, sort_keys=True)))
+# print(parsed['wordcount'])
--- axios_client_test.py
+++ axios_client_test.py
@@ -10,10 +10,10 @@ class TestAxiosClient(unittest.TestCase):
 
     def setUp(self):
         self.client = AxiosClient(STORY_STREAM_URL)
-        responses.add(responses.GET, 'https://test-url-0',
+        responses.add(responses.GET, 'https://test-url-0/stream/content/',
                       json={
                           'count': 15,
-                          'next': 'https://test-url-1',
+                          'next': 'https://test-url-1/stream/content/',
                           'previous': None,
                           'results': [
                               'article-1',
@@ -30,11 +30,11 @@ class TestAxiosClient(unittest.TestCase):
                       }
                       , status=200)
 
-        responses.add(responses.GET, 'https://test-url-1',
+        responses.add(responses.GET, 'https://test-url-1/stream/content/',
                       json={
                           'count': 15,
                           'next': None,
-                          'previous': 'https://test-url-0',
+                          'previous': 'https://test-url-0/stream/content/',
                           'results': [
                               'article-11',
                               'article-12',
@@ -45,22 +45,54 @@ class TestAxiosClient(unittest.TestCase):
                       }
                       , status=200)
 
-    @responses.activate
-    def test_get_results_returns_an_article(self):
-        results = self.client.get_articles(1)
-        self.assertEqual(results[0], 'article-1')
+        responses.add(responses.GET, 'https://test-url-0/content/article-1/',json={'wordcount': 300}, status=200)
+        responses.add(responses.GET, 'https://test-url-0/content/article-1/',json={'wordcount': 400}, status=200)
+        responses.add(responses.GET, 'https://test-url-0/content/article-2/',json={'wordcount': 500}, status=200)
+        responses.add(responses.GET, 'https://test-url-0/content/article-3/',json={'wordcount': 600}, status=200)
+        responses.add(responses.GET, 'https://test-url-0/content/article-4/',json={'wordcount': 700}, status=200)
+        responses.add(responses.GET, 'https://test-url-0/content/article-5/',json={'wordcount': 800}, status=200)
+        responses.add(responses.GET, 'https://test-url-0/content/article-6/',json={'wordcount': 900}, status=200)
+        responses.add(responses.GET, 'https://test-url-0/content/article-7/',json={'wordcount': 1000}, status=200)
+        responses.add(responses.GET, 'https://test-url-0/content/article-8/',json={'wordcount': 1100}, status=200)
+        responses.add(responses.GET, 'https://test-url-0/content/article-9/',json={'wordcount': 1200}, status=200)
+        responses.add(responses.GET, 'https://test-url-0/content/article-10/',json={'wordcount': 1300}, status=200)
 
     @responses.activate
-    def test_get_results_returns_2_articles(self):
-        results = self.client.get_articles(2)
-        self.assertEqual(len(results), 2)
-        self.assertEqual(results[0], 'article-1')
-        self.assertEqual(results[1], 'article-2')
+    def test_get_pages_returns_an_article(self):
+        results = self.client.get_pages(1)
+        self.assertEqual('article-1', results[0])
 
     @responses.activate
-    def test_get_results_returns_12_articles(self):
-        results = self.client.get_articles(12)
-        self.assertEqual(12, len(results))
-        self.assertEqual(results[10], 'article-11')
-        self.assertEqual(results[11], 'article-12')
+    def test_get_pages_returns_first_page_by_default(self):
+        results = self.client.get_pages()
+        self.assertEqual(len(results), 10)
+        self.assertEqual('article-1', results[0])
+        self.assertEqual('article-10', results[9])
 
+    @responses.activate
+    def test_get_pages_returns_first_page(self):
+        results = self.client.get_pages(1)
+        self.assertEqual(len(results), 10)
+        self.assertEqual('article-1', results[0])
+        self.assertEqual('article-10', results[9])
+
+    @responses.activate
+    def test_get_pages_returns_2_pages(self):
+        results = self.client.get_pages(2)
+        self.assertEqual(15, len(results))
+        self.assertEqual('article-11', results[10])
+        self.assertEqual('article-15', results[14])
+
+    @responses.activate
+    def test_get_pages_returns_max_available_pages(self):
+        results = self.client.get_pages(5)
+        self.assertEqual(15, len(results))
+        self.assertEqual('article-11', results[10])
+        self.assertEqual('article-15', results[14])
+
+    @responses.activate
+    def test_get_stories_length_returns_word_count(self):
+        stories = self.client.get_pages(1)
+        story_details = self.client.get_summary(stories)
+        self.assertEqual(300, story_details['article-1']['wordcount'])
+        # self.assertEqual(1, story_details['avg_reading_time'])
