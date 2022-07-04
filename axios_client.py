9c8d5faa6bc05c942623c3a5aed38bd69685e7bc
yasselpiloto <yasselpiloto@gmail.com>
2022-07-04 08:37:17 -0400

    package with docker

--- axios_client.py
+++ axios_client.py
@@ -1,18 +1,15 @@
 import requests
 
-# using 300 words per minute coefficient according to: https://irisreading.com/what-is-the-average-reading-speed/
-WORDS_PER_MINUTE = 300
-
 
 class AxiosClient:
-
-    def __init__(self, news_api_url):
-        self.news_api_url = news_api_url
+    # using 300 words per minute coefficient according to: https://irisreading.com/what-is-the-average-reading-speed/
+    WORDS_PER_MINUTE = 300
+    STORY_STREAM_URL = "https://api.axios.com/api/render"
 
     # gets the ids for the stories in the first n pages from the stream
     def get_story_ids(self, pages=1):
         results = []
-        next_page = f'{self.news_api_url}/stream/content/'
+        next_page = f'{self.STORY_STREAM_URL}/stream/content/'
 
         # iterates the first N pages of the story stream
         while len(results) < pages * 10 and next_page is not None:
@@ -24,24 +21,23 @@ class AxiosClient:
 
     # returns a dictionary with the wordcount and reading time for each story
     def get_story_summary(self, story_id):
-        response = requests.get(f'{self.news_api_url}/content/{story_id}/').json()
+        response = requests.get(f'{self.STORY_STREAM_URL}/content/{story_id}/').json()
         wordcount = 0
 
         for block in response['blocks']['blocks']:
             wordcount += len(block['text'].split())
 
-        words_per_minute = wordcount // WORDS_PER_MINUTE
+        words_per_minute = wordcount // self.WORDS_PER_MINUTE
 
         return {
-                'permalink': response['permalink'],
-                'headline': response['headline'],
-                'word_count': wordcount,
-                'reading_time': "<1" if words_per_minute == 0 else words_per_minute
-            }
+            'permalink': response['permalink'],
+            'headline': response['headline'],
+            'word_count': wordcount,
+            'reading_time': "<1" if words_per_minute == 0 else words_per_minute
+        }
 
     def get_content_summary(self, pages=1):
         story_ids = self.get_story_ids(pages)
-        print(story_ids)
         result = {}
 
         for story_id in story_ids:
--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-from word_count import WordCounter
-
-STORY_STREAM_URL = "https://api.axios.com/api/render/stream/content/"
+from axios_client import AxiosClient
+import json
+import argparse
 
 if __name__ == '__main__':
-    WordCounter(STORY_STREAM_URL).get_stream()
+
+    # parse arguments passed from standard input
+    parser = argparse.ArgumentParser(description="A news word counter")
+    parser.add_argument("pages", nargs="?", metavar="pages", type=int, help="Number of pages to analyze")
+    args = parser.parse_args()
+
+    pages = 1 if args.pages is None else args.pages
+
+    # print word count and reading time for each article in the first N pages
+    print(json.dumps(AxiosClient().get_content_summary(pages), indent=4, sort_keys=True))
